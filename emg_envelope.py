96f2fe2af5b2f17c76f5b9781352c51626def9df
suzie13 <sush.schandra@gmail.com>
2020-07-15 12:31:09 +0530

    renamed file

--- emg_envelope.py
+++ emg_envelope.py
@@ -1,20 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy 
 from scipy.signal import butter, filtfilt, savgol_filter, hilbert
-
-q2=pd.read_csv('/Volumes/Seagate Backup Plus Drive/NinaPro DB-2/EMG data/S1/S1.csv')
-
 from sklearn.preprocessing import MaxAbsScaler
-scaler=MaxAbsScaler()
-scaled=scaler.fit_transform(q2)
-q1=pd.DataFrame(scaled)
-
-q = q1[9,1462700:1502700]
-q_abs = numpy.absolute(q)
+import librosa
+import librosa.display
 
 #envelope functions    
-def emg_filter_bandpass(x, order=7, sRate=2000., cut=5., btype='low'):
+def emg_filter_bandpass(x, order=4, sRate=2000., cut=5., btype='low'):
     """ Forward-backward (high- or low-)pass filtering (IIR butterworth filter) """
     nyq = 0.5 * sRate
     low = cut / nyq
@@ -29,39 +26,129 @@ def calc_hilbert(signal):
     instantaneous_frequency = (numpy.diff(instantaneous_phase) / (2.0 * numpy.pi) * 650)
     return [amplitude_envelope, instantaneous_frequency]
 
-def calc_envelope(signal, freq=50, smooth=51):
+def calc_envelope(signal, freq=5., smooth=51):
     h = calc_hilbert(emg_filter_bandpass(signal, cut=freq))  # lowpass filter & hilbert transform
+    #h = numpy.array(h)
     sf = savgol_filter(h[0], smooth, 1, axis=0) # extra smoothing
     return sf  
 
-window_size = 200
-i = 0
-moving_averages = []
+def MAV(signal):
+    sig = calc_envelope(signal)
+    window_size = 500
+    i = 0
+    moving_averages = []
 
-while i < len(q_abs) - window_size + 1:
-    this_window = q_abs[i : i + window_size]
+    while i < len(sig) - window_size + 1:
+        this_window = sig[i : i + window_size]
+                
+        window_average = sum(this_window) / window_size
+        moving_averages.append(window_average)
+        i += 1
+    return moving_averages
 
-    window_average = sum(this_window) / window_size
-    moving_averages.append(window_average)
-    i += 50
+def sum_env(signal1, signal2):
+    env_sum = [signal1[i] + signal2[i] for i in range(len(signal1))] 
     
-'''FOR SEPERATE GRAPHS, USE THIS
+    return env_sum    
 
-fig, ax = plt.subplots(figsize=(13,4))
+def loader(uRange, lRange):
+    num = subject_num
+    data = numpy.loadtxt(f"{INPUT_FOLDER}S{num}.csv", delimiter = ',', unpack = True)
+    scaler = MaxAbsScaler()
+    scaled = scaler.fit_transform(data)
+    _scaled1 = numpy.absolute(scaled[8,lRange:uRange])
+    _scaled2 = numpy.absolute(scaled[9,lRange:uRange])
 
-ax.plot(q_abs, 'r', label='raw data')
-ax.plot(moving_averages, 'b', label='envelope')
-'''
+    '''    
+    for chan in range(1, channels+1):
+        _data = []
+        _data.append(numpy.absolute(scaled[8,lRange:uRange]))
+        chan += 1
+    '''    
+    return _scaled1, _scaled2
 
-fig, ax1 = plt.subplots(figsize=(13,4))
+def data_prep():
+    _data1, _data2 = loader(lRange, uRange)
+    
+    mav1 = MAV(_data1)
+    mav2 = MAV(_data2)
+    
+    return sum_env(mav1, mav2)
 
-ax1.plot(q_abs, 'r', label='raw data')
-ax2 = ax1.twiny()  # instantiate a second axes that shares the same x-axis
-ax2.plot(moving_averages, 'b', label='envelope')
+# In[2]:
 
-fig.tight_layout()  
-plt.legend(loc="upper left")
+def plot(data1, data2):
+    
+    #'''FOR SEPERATE GRAPHS, USE THIS
+    
+    fig, ax = plt.subplots(figsize=(10,4))
 
-#plt.savefig("/Volumes/Seagate Backup Plus Drive/NinaPro DB-2/EMG data/RAW_w200s50.jpg", Transparent = True)
+    #ax.plot(data1, 'r', label='raw data')
+    ax.plot(data2, 'b', label='envelope')
+    '''
+    fig, ax1 = plt.subplots(figsize=(10,4))
+    
+    ax1.plot(data1, 'r', label='raw data')
+    ax2 = ax1.twiny()  # instantiate a second axes that shares the same x-axis
+    ax2.plot(data2, 'b', label='envelope')
+    
+    fig.tight_layout()  
+    plt.legend(loc="upper left")
+    '''
+    #plt.savefig("/Volumes/Seagate Backup Plus Drive/NinaPro DB-2/EMG data/RAW_w200s50.jpg", Transparent = True)
+    plt.show()
+
+
+# In[20]:
+
+"""
+#The scipy spectogram
+from scipy import signal
+from scipy.fftpack import fftshift
+
+INPUT_FOLDER = '/Volumes/Seagate Backup Plus Drive/NinaPro DB-2/EMG data/'
+
+subject_num: int = 1
+channels: int = 2
+lRange = 1462700
+uRange = 1502700
+
+s1, s2 = loader(uRange, lRange)
+s1 = s1
+s2 = s2
+mav1 = MAV(s1)
+mav2 = MAV(s2)
+
+plt.figure()
+
+f,t, Sxx = signal.spectrogram(numpy.array(sum_env(mav1, mav2)[35000:39500]),fs=2000 ,return_onesided=True, mode='phase')
+
+plt.pcolormesh(t, f, Sxx, shading='gouraud')
+plt.ylabel('Frequency [Hz]')
+plt.xlabel('Time [sec]')
+#plt.savefig("C:/Users/hp/Downloads/spectogram.jpg")
 plt.show()
+"""
+
+# In[5]:
+#The librosa spectogram
+
+INPUT_FOLDER = '/Volumes/Seagate Backup Plus Drive/NinaPro DB-2/EMG data/'
+
+subject_num: int = 1
+channels: int = 2
+lRange = 1400000
+uRange = 1600000
+
+s1, s2 = loader(uRange, lRange)
+s1 = s1
+s2 = s2
+mav1 = MAV(s1)
+mav2 = MAV(s2)
+
+
+S = librosa.feature.melspectrogram(y=numpy.array(sum_env(mav1, mav2)[40000:84600]))
+librosa.display.specshow(S, x_axis='time', y_axis='log')
+plt.colorbar(format='%+2.0f dB')
+plt.ylim(0,100)
 
