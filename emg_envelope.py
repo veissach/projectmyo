c8169caf0f0a2704f88836efa5f8e147b42549e2
Vyshak Venkatesh <47595046+veissach@users.noreply.github.com>
2021-03-03 23:11:18 +0530

    Structured instance methods

--- emg_envelope.py
+++ emg_envelope.py
@@ -5,36 +5,37 @@ from scipy.signal import butter, filtfilt, savgol_filter, hilbert
 from sklearn.preprocessing import MaxAbsScaler
 
 class EmgData:#envelope functions  
-    path = '/Volumes/Seagate Backup Plus Drive/NinaPro DB-2/EMG data/'
     
-    def __init__(self, subject_num, channels, lRange, uRange, ):
+    
+    def __init__(self, lRange, uRange, subject_num: int = 1, channels: int = 2):
         self.subject_num = subject_num
         self.channels = channels
         self.lRange = lRange
         self.uRange = uRange
         
-    def emg_filter_bandpass(x, order=4, sRate=2000., cut=5., btype='low'):
+    def emg_filter_bandpass(self, x, order=4, sRate=2000., cut=5., btype='low'):
         """ Forward-backward (high- or low-)pass filtering (IIR butterworth filter) """
+        Shape = x.shape[0]
         nyq = 0.5 * sRate
         low = cut / nyq
-        b, a = butter(order, low, btype=btype, analog=False)
+        b, a = butter(4, low, btype=btype, analog=False)
         return filtfilt(b=b, a=a, x=x, axis=0, method='pad', padtype='odd',
-                        padlen=numpy.minimum(3 * numpy.maximum(len(a), len(b)), x.shape[0] - 1))
+                        padlen=numpy.minimum(3 * numpy.maximum(len(a), len(b)), Shape - 1))
     
-    def calc_hilbert(signal):
+    def calc_hilbert(self, signal): #preprocessing
         analytic_signal = hilbert(signal)
         amplitude_envelope = numpy.abs(analytic_signal)
         instantaneous_phase = numpy.unwrap(numpy.angle(analytic_signal))
         instantaneous_frequency = (numpy.diff(instantaneous_phase) / (2.0 * numpy.pi) * 650)
         return [amplitude_envelope, instantaneous_frequency]
     
-    def calc_envelope(self, signal, freq=5., smooth=51):
+    def calc_envelope(self, signal, freq=5., smooth=51): #preprocessing
         h = self.calc_hilbert(self.emg_filter_bandpass(signal, cut=freq))  # lowpass filter & hilbert transform
         #h = numpy.array(h)
         sf = savgol_filter(h[0], smooth, 1, axis=0) # extra smoothing
         return sf  
     
-    def MAV(self, signal):
+    def MAV(self, signal): 
         sig = self.calc_envelope(signal)
         window_size = 500
         i = 0
@@ -48,7 +49,7 @@ class EmgData:#envelope functions
             i += 1
         return moving_averages
     
-    def sum_env(signal1, signal2):
+    def sum_env(self, signal1, signal2):
         env_sum = [signal1[i] + signal2[i] for i in range(len(signal1))] 
         
         return env_sum    
@@ -69,21 +70,22 @@ class EmgData:#envelope functions
         '''    
         return _scaled1, _scaled2
     
-    def data_prep(self):
-        _data1, _data2 = self.loader(self.lRange, self.uRange)
+    def data_prep(self, path):
+        _data1, _data2 = self.loader(path)
         
-        mav1 = self.MAV(_data1)
-        mav2 = self.MAV(_data2)
+        mav1 = numpy.asarray(self.MAV(_data1))
+        mav2 = numpy.asarray(self.MAV(_data2))
         
         return self.sum_env(mav1, mav2)
     
-    def plot(data1, data2):
+    def plot(self, data1, data2):
+    #def plot(data1, data2):
         
         #'''FOR SEPERATE GRAPHS, USE THIS
         
         fig, ax = plt.subplots(figsize=(10,4))
     
-        #ax.plot(data1, 'r', label='raw data')
+        ax.plot(data1, 'r', label='raw data')
         ax.plot(data2, 'b', label='envelope')
         '''
         fig, ax1 = plt.subplots(figsize=(10,4))
@@ -99,8 +101,6 @@ class EmgData:#envelope functions
         plt.show()
     
     
-    
-    
     #subject_num: int = 1
     #channels: int = 2
     #lRange = 1462700
@@ -111,12 +111,23 @@ class EmgData:#envelope functions
     #s2 = s2
     #mav1 = numpy.array(MAV(s1))
     #mav2 = numpy.array(MAV(s2))
-    #sum_ = numpy.array(sum_env(mav1, mav2)[20000:6000])
+    #sum_ = numpy.array(sum_env(mav1, mav2)[2000:6000])
     
     #plot(s1, sum_env(mav1, mav2)[2000:6000])
     
+if __name__ == '__main__':
+    path = '/Volumes/Seagate Backup Plus Drive/NinaPro DB-2/EMG data/'
     
-                
+    e = EmgData(2000, 6000)
+    
+    s1, s2 = e.loader(path)
+    s1 = s1
+    s2 = s2
+    
+    e.plot(s1, e.data_prep(path))
+    
+    
+            
             
             
             
