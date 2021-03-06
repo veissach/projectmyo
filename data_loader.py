ceaaad26657def76d777eb112624a42e898acd11
Vyshak Venkatesh <47595046+veissach@users.noreply.github.com>
2021-03-06 21:20:08 +0530

    made testing easier

--- data_loader.py
+++ data_loader.py
@@ -1,3 +1,5 @@
 from processor import EmgData
 
-e = EmgData(lRange, uRange)
+e = EmgData(1462700, 1502700)
+d1, d2 = e.loader('/Volumes/Seagate Backup Plus Drive/NinaPro DB-2/EMG data/')
+e.plot(e.MAV(d2))
