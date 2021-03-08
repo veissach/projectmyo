887fddeba01e29722af78001e1c6db35e10d1040
Vyshak Venkatesh <47595046+veissach@users.noreply.github.com>
2021-03-08 22:17:02 +0530

    mod

--- data_loader.py
+++ data_loader.py
@@ -2,4 +2,4 @@ from processor import EmgData
 
 e = EmgData(1462700, 1502700)
 d1, d2 = e.loader('/Volumes/Seagate Backup Plus Drive/NinaPro DB-2/EMG data/')
-e.plot(e.MAV(d2))
+e.plot(e.calc_envelope(d2))
