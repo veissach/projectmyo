98a94d74b28e371e2d0c87ddee4239c49ebb71c8
Vyshak Venkatesh <47595046+veissach@users.noreply.github.com>
2021-03-13 21:06:36 +0530

    bug fixes

--- model.py
+++ model.py
@@ -5,13 +5,13 @@ import pandas as pd
 from processor import preprocessing
 
 def prep_data(path):
-    e = preprocessing(0, 10000)
+    e = preprocessing(0, 100000)
     d1, d2, d3, d4 = e.loader(path)
     d1 = e.calc_envelope(d1)
     d2 = e.calc_envelope(d2)
     d3 = e.calc_envelope(d3)
     d4 = e.calc_envelope(d4)
-    Label = np.array(pd.read_csv(f"{path}S1_labels.csv", engine = 'python')[0:10000])
+    Label = np.array(pd.read_csv(f"{path}S1_labels.csv", engine = 'python')[0:100000])
     #L = Labels
     #Labels = np.reshape(Label, (40000,)
     #e.plot(e.calc_envelope(d2))
@@ -22,7 +22,6 @@ def prep_data(path):
     
 class KNN:
     
-        
     def __init__(self, path):
         d1, d2, d3, d4, Labels = prep_data(path)
         self.d1 = d1
@@ -51,8 +50,6 @@ class KNN:
         accuracy = clf.score(X_test, y_test)
         print('accuracy =', accuracy)
         
-k = KNN('/Volumes/Seagate Backup Plus Drive/NinaPro DB-2/EMG data/')
-k.train()
         
 #test_signal = np.array([[0.3308264649854933, 0.2381490536075344, 0.1997658380042437, 0.24130765822373523]])
 #prediction = clf.predict(test_signal)
@@ -61,3 +58,7 @@ k.train()
 
 #ind = 1165997
 #print(d1[ind], d2[ind], d3[ind], d4[ind])
+
+class CNN:
+    
+    def __init__(self):
